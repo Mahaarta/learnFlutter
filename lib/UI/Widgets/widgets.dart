9295eae967cfeedd2a21d92bcbdd5a131661ac17
Minata P Mahaarta <minataputra@gmail.com>
2023-01-27 17:49:12 +0700

    Coming Soon Section
    Add:
    1. coming_soon_card.dart
    	- widget
    
    Changes:
    1. movie_page.dart
    2. widgets.dart

--- lib/UI/Widgets/widgets.dart
+++ lib/UI/Widgets/widgets.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:learn_flutter/Models/models.dart';
 import 'package:learn_flutter/Shared/shared.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -8,3 +7,4 @@ part 'selectable_box.dart';
 part 'movie_card.dart';
 part 'rating_stars.dart';
 part 'browse_button.dart';
+part 'coming_soon_card.dart';
